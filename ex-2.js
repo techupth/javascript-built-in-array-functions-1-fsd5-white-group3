ca2c15c2b1f2b1ccc7778dbb036c7a8a76f5c35c
Pacharatnkp <pachara.pplzz@gmail.com>
2023-12-04 12:43:50 +0700

    feat: Complete assignment

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,8 @@ let words = ["apple", "cat", "dog", "elephant"];
 
 function get5CharWords(words) {
   // Start coding here
+  const result = words.filter(word => word.length >= 5);
+  return result;
 }
 
 const result = get5CharWords(words);
